1768b9b44fac64a8dca71eb28733ed6524ca4390
Damian Aenima <aenimalabs@gmail.com>
2020-08-18 15:06:21 -0400

    segundo

--- app/src/main/java/com/example/persistenciabasedatos_18agosto/TaskDataBase.kt
+++ app/src/main/java/com/example/persistenciabasedatos_18agosto/TaskDataBase.kt
@@ -1,11 +1,37 @@
 package com.example.persistenciabasedatos_18agosto
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import java.security.AccessControlContext
 
 @Database(entities = arrayOf(Task::class), version = 0, exportSchema = false)
 abstract class TaskDataBase : RoomDatabase(){
 
 
     abstract fun getTaskDAO():TaskDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TaskDataBase? = null
+
+        fun getDataBase(context: Context): TaskDataBase{
+            val tempInstance = INSTANCE
+            if (tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDataBase::class.java,
+                    "word_database").build()
+                INSTANCE = instance
+                return instance
+
+            }
+
+
+        }
+    }
 }
